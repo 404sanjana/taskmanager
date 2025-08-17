dcbb773e275bed9a6535749373c356a485b7aa6d
Sanjana <sanjanagudibandi09@gmail.com>
2025-08-17 13:47:01 +1000

    BTA-8  Sub Task 1.2: Implement input validation for the signup fields

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-//create an instance
+//create an instance to ip address
 const axiosInstance = axios.create({
   baseURL: 'http://13.55.53.130:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
